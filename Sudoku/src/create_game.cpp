d736b7e568aa5c000aac139faa8646ad9c2570d0
CatBitesTail <1810269887@qq.com>
2023-06-22 16:40:40 +0800

    完成create_game，测试solve_game正确

--- Sudoku/src/create_game.cpp
+++ Sudoku/src/create_game.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <ctime>
 #include "create_game.h"
 #include "board.h"
@@ -15,12 +16,12 @@ int getRand(int min, int max) {
 void create_game(int n) {
     srand((int) time(nullptr));
 
-    int blank_num = 10;                  // 挖空个数
+    //TODO 根据命令行参数中难度、挖空数量范围来设置blank_num
+    int blank_num = 20;                  // 挖空个数
     for (int i = 0; i < n; i++) {
-        //TODO 怎么知道已经有多少个终局，从而设置getRand的max，暂定从1~100中选
-        int final_no = getRand(0, countLines(FINAL_PATH)/10);
+        int final_no = getRand(0, countLines(FINAL_PATH) / 10);
         printf("%d:%d\n", i, final_no);
-//        game_generate(final_no, blank_num);
+        game_generate(final_no, blank_num);
     }
 }
 
@@ -38,36 +39,31 @@ void game_generate(int final_no, int blank_num) {
     int hole_block_num = blank_num / 9;//每个九宫格挖空数量
     int hole_remain_num = blank_num % 9;//剩余的挖空数量
 
+    int x, y;//要挖空的位置
+    int temp, dot;
     //每个九宫格随机掏空hole_block_num个
     for (int k: base) {
-        int i, j; // 3*3里面掏的位置
-        int temp,dot;
-        while (hole_block_num--) {        //挖hole_block_num个空
+        int num = hole_block_num;
+        while (num--) {        //挖hole_block_num个空
             do {
-                temp=rand()%9;
+                temp = rand() % 9;
                 dot = k + offset[temp];
-                i = dot / 9;
-                j = dot % 9;
-            }while (board.arr[i][j] == 0);
-            board.arr[i][j]=0;
+                x = dot / 9;
+                y = dot % 9;
+            } while (board.arr[x][y] == 0);
+            board.arr[x][y] = 0;
         }
     }
 
-    // 已经掏空了18个
-//    int others;
-//    others = 12 + rand() % 31;//再掏12-41个就可以了
-//    while (others--) {
-//        int k = rand() % 81;
-//        int i = k / 9;
-//        int j = k % 9;
-//
-//        if (board.arr[i][j] != '0'){
-//            board.arr[i][j] = '0';
-//        }else{
-//            others++;
-//        }
-//    }
-
+    //再随机挖hole_remain_num个空
+    while (hole_remain_num--) {
+        do {
+            dot = rand() % 81;
+            x = dot / 9;
+            y = dot % 9;
+        } while (board.arr[x][y] == 0);
+        board.arr[x][y] = 0;
+    }
 
     board.writeBoard(GAME_PATH);
 }
--- Sudoku/test.cpp
+++ Sudoku/test.cpp
@@ -1,10 +1,13 @@
 #include "board.h"
+#include "solve_game.h"
 using namespace std;
 
 int main(){
-    Board b;
-    b.readBoard("sudoku_final.txt",1);
-    b.output();
+//    Board b;
+//    b.readBoard("sudoku_final.txt",1);
+//    b.output();
+
+    solve("sudoku_game.txt");
 
     return 0;
 }
